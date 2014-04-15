d7227f170da921639b4f120abbaa50778c57f34c
manikk <manikk.h@gmail.com>
2014-04-15 13:04:46 +0530

    License changed to GPLv3

---
+++ src/widget.h
@@ -0,0 +1,39 @@
+/*****************************************************************************
+ * widget.h
+ *
+ * Created: 15/04/2014 2014 by manikk
+ *
+ * Copyright 2014 Thamizha. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *
+ * Authors :
+ *
+ *****************************************************************************/
+
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Widget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // WIDGET_H
