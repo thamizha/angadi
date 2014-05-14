9c7fe0d3ca11d893b45227a5f69cb65ce2d48aff
mohanrex <mohanraj.hunk@live.com>
2014-05-14 21:34:42 +0530

    minor ui changes in unpaid bill report

--- src/unpaidbillreport.cpp
+++ src/unpaidbillreport.cpp
@@ -9,7 +9,7 @@ UnpaidBillReport::UnpaidBillReport(QWidget *parent) :
     ui->setupUi(this);
 
     fromFilter = "2014-01-01";
-    toFilter = "2015-01-01";
+    toFilter = QDate::currentDate().toString("yyyy-MM-dd");
     invoiceNoFilter = "";
     totalFilter = "";
     customerFilter = "";
@@ -21,6 +21,11 @@ UnpaidBillReport::UnpaidBillReport(QWidget *parent) :
     billModel->setTable("bill");
     billModel->setEditStrategy(QSqlTableModel::OnManualSubmit);
     billModel->setRelation(3, QSqlRelation("customers", "id", "name"));
+    billModel->setHeaderData(1, Qt::Horizontal,QObject::tr("Invoice No"));
+    billModel->setHeaderData(2, Qt::Horizontal,QObject::tr("Date"));
+    billModel->setHeaderData(3, Qt::Horizontal,QObject::tr("Customer Name"));
+    billModel->setHeaderData(6, Qt::Horizontal,QObject::tr("Bill Amount"));
+    billModel->setHeaderData(7, Qt::Horizontal,QObject::tr("Balance"));
     filter = "paidStatus = 'U'";
     billModel->setFilter(filter);
     billModel->select();
@@ -34,14 +39,25 @@ UnpaidBillReport::UnpaidBillReport(QWidget *parent) :
     ui->tableView->verticalHeader()->setVisible(true);
     ui->tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
     ui->tableView->setSelectionMode(QAbstractItemView::SingleSelection);
-    ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
-    ui->tableView->resizeRowsToContents();
+//    ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableView->horizontalHeader()->setVisible(true);
+    ui->tableView->setColumnWidth(1,100);
+    ui->tableView->setColumnWidth(2,190);
+    ui->tableView->setColumnWidth(3,235);
+    ui->tableView->setColumnWidth(6,200);
+    ui->tableView->setColumnWidth(7,200);
     ui->tableView->setColumnHidden(0,true);
+    ui->tableView->setItemDelegateForColumn(1,new RightAlignDelegate);
+    ui->tableView->setItemDelegateForColumn(2,new TimeEditDelegate("dd/MM/yyyy"));
+    ui->tableView->setItemDelegateForColumn(6,new RightAlignDelegate);
+    ui->tableView->setItemDelegateForColumn(7,new RightAlignDelegate);
     ui->tableView->setColumnHidden(4,true);
     ui->tableView->setColumnHidden(5,true);
     for (int i = 8; i<billProxy->columnCount(); ++i)
         ui->tableView->setColumnHidden(i,true);
     calTotal();
+    ui->dateEditTo->setDate(QDate::currentDate());
+    ui->lineEditTotalBalance->setEnabled(false);
 }
 
 UnpaidBillReport::~UnpaidBillReport()
@@ -88,7 +104,7 @@ void UnpaidBillReport::on_lineEditBalance_textChanged(const QString &arg1)
 
 void UnpaidBillReport::setModel()
 {
-    filter = "paidStatus = 'U' and invoiceNo like '%"+invoiceNoFilter+"%' and totalAmount like '%"+totalFilter+"%' and relTblAl_3.name = '"+customerFilter+"' and dueAmount like '%"+balanceFilter+"%' and invoiceDate >= '"+fromFilter+"' and invoiceDate <= '"+toFilter+"'";
+    filter = "paidStatus = 'U' and invoiceNo like '%"+invoiceNoFilter+"%' and totalAmount like '%"+totalFilter+"%' and relTblAl_3.name like '%"+customerFilter+"%' and dueAmount like '%"+balanceFilter+"%' and invoiceDate >= '"+fromFilter+"' and invoiceDate <= '"+toFilter+"'";
     billModel->setFilter(filter);
     billModel->select();
     calTotal();
@@ -103,4 +119,5 @@ void UnpaidBillReport::calTotal()
         totalBalance=totalBalance+billRecord.value(7).toInt();
     }
     ui->lineEditTotalBalance->setText(QString::number(totalBalance));
+    ui->tableView->resizeRowsToContents();
 }
