fa84420a99698845b078f0724d4cd7b5a0242586
vdhanasekaran <vijay.kuruntham@gmail.com>
2014-05-15 21:42:41 +0530

    Splash screen added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,20 @@
 #include <angadiapp.h>
-#include <QWidget>
-#include <QtWidgets>
-#include <QWindow>
 #include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     AngadiApp a(argc, argv);
 
-//    QPixmap pix(":/images/icons/about_splash.png");
-//    QSplashScreen splash(pix);
-//    splash.show();
+    QPixmap pix(":/images/icons/about_splash.png");
+    QSplashScreen splash(pix);
+    splash.show();
 
-//    #ifdef WIN32
-//    // Windows code here
-//        Sleep(5000);
-//    #else
-//    // UNIX code here
-//        struct timespec ts = { 5000 / 1000, (5000 % 1000) * 1000 * 1000 };
-//        nanosleep(&ts, NULL);
-//    #endif
+    QTime dieTime= QTime::currentTime().addSecs(3);
+    while( QTime::currentTime() < dieTime )
+    QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
 
-//    splash.hide();
-
+    splash.hide();
     a.start();
+
     return a.exec();
 }
